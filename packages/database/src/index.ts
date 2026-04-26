645c9f93d37b5f35b783d9e948a68c1c037c818e
shashank-poola <shashankpoola123@gmail.com>
2026-04-26 12:02:34 +0530

    feat: prisma latest version init

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,6 +1,15 @@
+import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const prisma = new PrismaClient();
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+    throw new Error("DATABASE_URL is not set in env")
+};
+
+const adapter = new PrismaPg(connectionString);
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
 
